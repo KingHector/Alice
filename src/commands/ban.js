4ac98622531ccdb1cb48a1c347cb00e60cb0ebed
Hector Angelopoulos <hectoragg2004@gmail.com>
2021-09-10 13:13:33 +0300

    Finished !ban command

--- src/commands/ban.js
+++ src/commands/ban.js
@@ -1,3 +1,8 @@
+const config = require('../config.json')
+const { MessageEmbed } = require('discord.js')
+
+const prefix = config['Main-Settings']['Command-Prefix']
+
 module.exports = 
 {
     name: "ban",
@@ -6,6 +11,7 @@ module.exports =
     async execute(client, message, args, Discord)
     {
         const member = message.mentions.users.first()
+        const reason = message.content.slice(prefix.length + 2).slice('ban'.length).slice(args[0].length)
 
         if (message.member.permissions.has('BAN_MEMBERS'))
         {
@@ -15,8 +21,9 @@ module.exports =
                 
                 if (!targetedMember.permissions.has('ADMINISTRATOR') || targetedMember.user.bot)
                 {
-                    targetedMember.ban({reason: args[1] })
+                    targetedMember.ban({ reason: args[1] })
                     message.channel.send(':hammer: Successfully banned <@' + member + '>.')
+                    banLog(client, member, message, reason)
                 }
                 else //Member is Admin
                     message.channel.send('**You cannot ban this member.**')
@@ -25,4 +32,24 @@ module.exports =
                 message.channel.send(':x: **Invalid usage. Use !ban <user> __<reason>__.**')  
         }
     }
+}
+
+function banLog(client, member, message, reason)  
+{
+    const loggingChannel = client.channels.cache.find(channel => channel.name === config['Something-Settings']['Logging-Channel'])
+    const date = new Date()    
+
+    const banAddLog = new MessageEmbed()
+        .setColor('#FFFF00')
+        .setTitle('BAN - Case #')
+        .setFields
+        (
+            { name: 'User', value: `<@${member.id}>`, inline: true},
+            { name: 'Moderator', value: `<@${message.author.id}>`, inline: true },
+            { name: 'Reason', value: '```' + `${reason} ` + '```'}
+        )
+        .setThumbnail(config['Graphical-Settings']['Ban-Image'])
+        .setFooter('Case created on ' + date.toUTCString())
+       
+    client.channels.cache.get(loggingChannel['id']).send({ embeds: [banAddLog] })   
 }
--- src/events/ready.js
+++ src/events/ready.js
@@ -1,19 +1,4 @@
-const config = require('../config.json')
-const { MessageEmbed } = require('discord.js')
-
 module.exports = (Discord, client, message) =>
 {
     console.log('Bot is Online!')
-
-    const loggingChannel = client.channels.cache.find(channel => channel.name === config['Something-Settings']['Logging-Channel'])
-    const date = new Date()
-
-    const banAddLog = new MessageEmbed()
-        .setColor('#FFFF00')
-        .setTitle('BAN - Case #')
-        .setFields()
-        .setThumbnail(config['Graphical-Settings']['Ban-Image'])
-        .setFooter('Case created on ' + date.toUTCString())
-       
-    client.channels.cache.get(loggingChannel['id']).send({ embeds: [banAddLog] }) 
 }
