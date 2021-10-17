55677e0f5c48a496ee34c45bd8e24e62949570a1
Hector Angelopoulos <hectoragg2004@gmail.com>
2021-10-17 17:47:35 +0300

    Readded 2 semicolons in main Added !case Updated !inquire to work only when database is online

---
+++ src/commands/case.js
@@ -0,0 +1,30 @@
+const { MessageEmbed } = require('discord.js')
+const sql = require('../typDiscordBot').getsql
+
+module.exports = 
+{
+    name: 'case',
+    description: 'Shows a punishment case specified.',
+
+    async execute(client, message, args, Discord)
+    {
+        if (sql.state === 'authenticated')
+        {
+            if (message.member.permissions.has('ADMINISTRATOR'))
+            {
+                if (args.length >= 1)
+                {
+                    sql.query(`SELECT * FROM discordlogs WHERE CaseID = ${args[0]}`, function(err, caseNo, fields) 
+                    {
+                        const embedJSON = JSON.parse(caseNo[0]['Embed'])
+                        message.channel.send({ embeds: [embedJSON] })
+                    })
+                }
+                else //No case specified
+                    message.channel.send(':x: **Invalid usage. Use !case <caseNumber>.**')
+            }
+        }
+        else //Database offline
+            message.channel.send(':x: **Cannot receive cases from database.**')
+    }   
+}
--- src/typDiscordBot.js
+++ src/typDiscordBot.js
@@ -26,12 +26,12 @@ sql.connect(error =>
 
 module.exports = { getsql: sql}            
 
-client.commands = new Discord.Collection()
-client.events = new Discord.Collection()
+client.commands = new Discord.Collection();
+client.events = new Discord.Collection();
 
 ['commandHandler', 'eventHandler'].forEach(handler => 
     {
         require(`./handlers/${handler}`)(client, Discord)
-    })     
+    });    
 
 client.login(config['Main-Settings']['Bot-Token'])
