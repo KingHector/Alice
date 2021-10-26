b2538b1048898a4978e3d7249b1137e7abbbdd32
Hector Angelopoulos <hectoragg2004@gmail.com>
2021-10-26 14:34:59 +0300

    Finished welcome plugin

--- src/events/guildMemberAdd.js
+++ src/events/guildMemberAdd.js
@@ -13,9 +13,29 @@ module.exports = (Discord, client, add) =>
         }    
         catch (error)
         {
-            console.log(chalk.red('[ERROR] Plugin: Welcome/Message-On-Join is not configured properly.'))
+            console.log(chalk.red('[ERROR] Plugin Welcome/Message-On-Join is not configured properly.'))
         }
     }
+
+    if (welcomeConfig['Give-Role-To-New-User']['Enabled'])
+    {
+        try
+        {
+            const member = add.user.id
+            const targetedMember = add.guild.roles.guild.members.cache.get(member)
+            const rolesToAdd = welcomeConfig['Give-Role-To-New-User']['Roles']
+            
+            for (let role of rolesToAdd)
+            {
+                const t = add.guild.roles.cache.find(r => r.name === `${role}`)
+                targetedMember.roles.add(t)
+            }
+        }   
+        catch (error)
+        {
+            console.log(chalk.red('[ERROR] Plugin Welcome/Give-Role-To-New-User is not configured properly.'))
+        }
+    }    
 
     if (welcomeConfig['Private-Message-On-Join']['Enabled'])
     {
